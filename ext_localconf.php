423f0a47c72627d9d4f71174aa92391a6ca0d68a
pulponair <pulponair@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-05-04 20:30:11 +0000

    Cleanup.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/nh_tvdragndrop/trunk@19886 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,5 +2,5 @@
 if (!defined('TYPO3_MODE')) die ('Access denied!');
 
 $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/templavoila/mod1/index.php'] =
-	t3lib_extMgm::extPath('nh_tvdragndrop').'class.ux_tx_templavoila_module1.php';
+	t3lib_extMgm::extPath('nh_tvdragndrop') . 'class.ux_tx_templavoila_module1.php';
 ?>
