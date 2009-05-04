c9753ac2f444814156a14270d63669c939ca33b0
pulponair <pulponair@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-05-04 20:27:41 +0000

    Removed bogus whitespace.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/nh_tvdragndrop/trunk@19883 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,7 +1,6 @@
-
-<?PHP
+<?php
 if (!defined('TYPO3_MODE')) die ('Access denied!');
-$TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/templavoila/mod1/index.php'] = t3lib_extMgm::extPath('nh_tvdragndrop').'class.ux_tx_templavoila_module1.php';
 
+$TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/templavoila/mod1/index.php'] =
+	t3lib_extMgm::extPath('nh_tvdragndrop').'class.ux_tx_templavoila_module1.php';
 ?>
-
